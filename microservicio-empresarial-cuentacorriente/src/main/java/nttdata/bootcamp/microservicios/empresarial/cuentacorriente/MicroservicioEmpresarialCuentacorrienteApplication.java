6305f568a098fe324b2934c98252b2868beb3458
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 11:48:32 -0500

    segundo commit

---
+++ microservicio-empresarial-cuentacorriente/src/main/java/nttdata/bootcamp/microservicios/empresarial/cuentacorriente/MicroservicioEmpresarialCuentacorrienteApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.empresarial.cuentacorriente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEmpresarialCuentacorrienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioEmpresarialCuentacorrienteApplication.class, args);
+	}
+
+}
---
+++ microservicio-empresarial-cuentacorriente/src/test/java/nttdata/bootcamp/microservicios/empresarial/cuentacorriente/MicroservicioEmpresarialCuentacorrienteApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.empresarial.cuentacorriente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEmpresarialCuentacorrienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
